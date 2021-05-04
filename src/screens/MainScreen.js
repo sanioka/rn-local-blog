20ec52e18b9a9665756e9a4a60390e378f737079
AIvanov <isanioka@gmail.com>
2021-05-04 13:42:45 +0300

    post component / flatlist

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
+import { DATA } from '../data';
+import { Post } from "../components/Post";
+
 
 export const MainScreen = ({ navigation }) => {
   const goToPost = () => {
     navigation.navigate('Post');
   }
   return (
-    <View style={styles.center}>
-      <Text>Main screen</Text>
-      <Button title='go to post' onPress={goToPost}/>
+    <View style={styles.wrapper}>
+      <FlatList
+        data={DATA}
+        keyExtractor={post => post.id.toString()}
+        renderItem={({ item }) => <Post post={item}/>}
+      />
     </View>
   )
-}
+};
 
 MainScreen.navigationOptions = {
   headerTitle: 'My blog',
@@ -23,5 +29,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  wrapper: {
+    padding: 10,
   }
 })

3eaa8a9ca975816620599dc5910aacf03256a48e
AIvanov <isanioka@gmail.com>
2021-05-04 17:57:08 +0300

    created Redux boilerplate

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import { Provider } from 'react-redux';
+
 import { bootstrap } from "./src/bootstrap";
 import { AppNavigation } from "./src/navigation/AppNavigation";
+import store from './src/store'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -16,6 +18,8 @@ export default function App() {
   }
 
   return (
-    <AppNavigation />
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
   );
 }

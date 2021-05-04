62b9d2bef8377b8f34665e74b73cfdc233264127
AIvanov <isanioka@gmail.com>
2021-05-04 11:51:22 +0300

    load fonts

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return <AppLoading
+      startAsync={ bootstrap }
+      onFinish={() => setIsReady(true)}
+      onError={error => console.log(error)}
+    />
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>

87cf3e5fc4dd12c233060c61a2f808abcabaaf4b
AIvanov <isanioka@gmail.com>
2021-05-04 13:22:08 +0300

    theme with color android / ios

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,6 +2,8 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { MainScreen } from "../screens/MainScreen";
 import { PostScreen } from "../screens/PostScreen";
+import { THEME } from "../theme";
+import { Platform } from "react-native";
 
 const PostNavigator = createStackNavigator({
   Main: MainScreen,
@@ -9,7 +11,13 @@ const PostNavigator = createStackNavigator({
     screen: PostScreen,
   }
 }, {
-  initialRouteName: 'Main'
+  initialRouteName: 'Main',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'ios' ? '#fff' : THEME.MAIN_COLOR,
+    },
+    headerTintColor: Platform.OS === 'ios' ? THEME.MAIN_COLOR : '#fff',
+  }
 })
 
 export const AppNavigation = createAppContainer(PostNavigator);

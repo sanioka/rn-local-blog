bdf855a47ff76ad70b338b82efc35da8db45d8c3
AIvanov <isanioka@gmail.com>
2021-05-04 19:28:31 +0300

    create post, part1

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, Image, Button, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { THEME } from "../theme";
 
 export const CreateScreen = (props) => {
+  const [text, setText] = useState('')
+
+  const createPostHandler = () => {
+
+  }
+
   return (
-    <View style={styles.center}>
-      <Text>Create screen</Text>
-    </View>
+    <ScrollView style={styles.wrapper}>
+      <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+        <View>
+          <Text style={styles.title}>Create new post</Text>
+          <TextInput
+            style={styles.textarea}
+            placeholder='Input text here'
+            value={text}
+            onChangeText={setText}
+            multiline
+          />
+          <Image
+            style={styles.image}
+            source={{uri: 'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg'}}/>
+          <Button title='Create post' color={THEME.MAIN_COLOR} onPress={createPostHandler}/>
+        </View>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   )
 }
 
@@ -28,9 +50,22 @@ CreateScreen.navigationOptions = ({ navigation }) => ({
 })
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  wrapper: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'open-regular',
+    marginVertical: 10,
+  },
+  textarea: {
+    padding: 10,
+    marginBottom: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
   }
 })
